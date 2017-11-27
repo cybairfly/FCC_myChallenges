f8583feee324c7697963544fd8c498cd5782bc6a
Tobey <cyberfly@email.cz>
2017-11-27 11:33:09 +0100

    Add files via upload

---
+++ Sort-Arrays-with-sort.js
@@ -0,0 +1,10 @@
+
+var array = [1, 12, 21, 2];
+
+// Only change code below this line.
+
+array.sort(
+  function(first, second) {
+    return second - first;
+  }
+);
---
+++ Word-Blanks.js
@@ -0,0 +1,12 @@
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = "";
+  // Your code below this line
+  result = "This " + myNoun + " test " + myAdjective + " kinda " + myVerb + " sucks " + myAdverb + " maybe.";
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("one", "nice", "minor", "example");
